38fc72bb65a3f21c41bb5f187ee276a56be4a7c2
Nishantpaliwal19 <43276854+Nishantpaliwal19@users.noreply.github.com>
2019-10-12 01:24:33 +0530

    added a hidden layer

--- ann.py
+++ ann.py
@@ -33,6 +33,9 @@ classifier.add(Dense(units = 6, kernel_initializer = 'uniform', activation = 're
 
 classifier.add(Dense(units = 6, kernel_initializer = 'uniform', activation = 'relu'))
 
+classifier.add(Dense(units = 3, kernel_initializer = 'uniform', activation = 'relu'))
+
+
 classifier.add(Dense(units = 1, kernel_initializer = 'uniform', activation = 'sigmoid'))
 
 classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
